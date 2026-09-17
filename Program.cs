0d13fe970717e952693a8bd663a4a2a0077ef763
AbelChocca <140349180+AbelChocca@users.noreply.github.com>
2026-09-17 08:48:18 -0500

    Add Program class with Main method for banking operations

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using ConsoleBankingApp.Models;
+using ConsoleBankingApp.Services;
+
+namespace ConsoleBankingApp;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        IAccountService accountService = new AccountService();
+
+        BankAccount account1 = new("001");
+        BankAccount account2 = new("002");
+
+        accountService.Deposit(account1, 1000m);
+
+        accountService.Transfer(
+            account1,
+            account2,
+            250m
+        );
+
+        Console.WriteLine($"Account 1: {account1.Balance}");
+        Console.WriteLine($"Account 2: {account2.Balance}");
+    }
+}
